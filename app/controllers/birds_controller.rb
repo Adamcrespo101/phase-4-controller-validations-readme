9f97ea1bb5cbf4dfc6e998c0e59bc9861fee7817
Adam Crespo <alc49@LAPTOP-7KRCGAGF.localdomain>
2022-04-26 13:44:11 -0400

    commit tree 242bb2776651b5069e57e8c86e3319471b88a98f

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,8 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # added rescue_from
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
 
   # GET /birds
   def index
@@ -9,7 +12,7 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
   end
 
